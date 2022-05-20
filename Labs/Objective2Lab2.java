b45f9e8e3f052ab5cbebee9e850612f75e8f4e0e
Dylan Stavena <stavena.dylan@gmail.com>
2022-05-20 17:05:40 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Dylan Stavena";
+    int age = 27;
+    String state = "Texas";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
